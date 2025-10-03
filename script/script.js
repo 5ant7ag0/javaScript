d376aad5517ba8b4fb706e37e52913b45be45534
Santiago Tenensaray <santytenensaray@gmail.com>
2025-10-03 11:08:56 -0500

    Condicionales

--- script/script.js
+++ script/script.js
@@ -49,10 +49,77 @@ function mostrarNombre(){
     let nombre="Pedro" //let va estar disponible dentro de la funcion
     document.writeln("<br>");
     document.writeln("<br>");
-    document.writeln("Mi nombre es: "+nombre);
+    document.writeln
 }
 
 //Formas de llamar a una funcion
 
 mostrarNombre();
 document.writeln("<br> <br> Mi otro nombre: "+nombre);
+
+// funcion que recibe parametros
+function sumar(a,b){
+    let resultado;
+    resultado=a+b;
+    resultado=a+b;
+    return resultado;
+
+}
+
+//Llamamos a la funcion 
+var suma=sumar(40,50);
+document.writeln("<br><br> La suma es: "+suma);
+/*OPERADOERES ARITMETICOS
++ Suma 
+- resta
+* Multiplicacion 
+/ Division
+% Modulo: residuo de una division 
+++ Incremento
+-- Decremento 
+
+Operadores de asignacion:
+Estos operadores se utilizan para asignar valores a las variables.
+= signacion simple
++= Asignacion con suma (y+= b es equivalente y y=y+b)
+-= Asiganacion de resta (y -=b es equivalente a y=y-b)
+=== Igualdad estricta (Compara valor y tipo 6) 6==== "6": no es igual
+
+OPERADORES DE COMPARACION:
+Estos operadores se utilizan para comparar valores y devuelven un valor booleano (True o false).
+== Igualdad (Compara solo valor)
+!= Desigualdad (Compara solo el valor)
+> Mayor que
+> Menor que
+<= Mayor o igual que
+>= Menor o igual que
+
+OPERADORES LOGICOS 
+Estos operadores se utilizan para combinar expresiones booleanas
+
+&& AND devuelve true si ambas condiciones son verdaderas 
+|| OR Devuelve true si al menos una condicion es verdaddera 
+! NOT Invierte el valor de una condion*/
+
+
+//La condicional nos permite ejecutar un bloque de codigo si se cumole una condicion.
+
+/*SINTAXIS:
+if(condicion){
+    Bloque de codigo a ejecutar si la condicion es verdaddera
+    }else{
+        Bloque de codigo a ejecutar si la condicion es falsa
+    }
+        */
+//
+
+
+//crear un script donde se verifique la edad entre dos personas 
+var edadElvis=29;
+var edadJustin=40;
+if(edadElvis>edadJustin){
+    document.writeln("<br> Elvis es mayor que Justin");
+    
+}else{
+    document.writeln("<br> Justin es mayor que Elvis");
+}
